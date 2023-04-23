bf3f1b6d9b70e5e504659c40fa0aba58e375eec2
Arley Souto <macx15@gotmail.com>
2023-04-23 14:49:04 -0300

    armzenar peças capturadas

--- chese-system/src/chess/ChessMatch.java
+++ chese-system/src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -11,6 +14,8 @@ public class ChessMatch {
     private int turn;
     private Color currentPlayer;
     private Board board;
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     // quem tem definir o tamanho do tabuleiro de xadrez é essa classe
     public ChessMatch() {
@@ -58,9 +63,14 @@ public class ChessMatch {
 
     public Piece makeMove(Position source, Position target) {
         Piece p = board.removePiece(source);
-        Piece capturePiece = board.removePiece(target);
+        Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
-        return capturePiece;
+
+        if (capturedPiece != null) {
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
+        return capturedPiece;
     }
 
     private void validadeSourcePosition(Position position) {
@@ -83,11 +93,12 @@ public class ChessMatch {
 
     private void nextTurn() {
         turn++;
-        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.BLACK;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     public void initialSetup() {
